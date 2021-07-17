cdb13e9137553e1cb98de4a6682f4d486bf9ffe9
jiwon.cha <wonny.jw@gmail.com>
2021-07-17 15:50:57 +0900

    Update view

--- api/config.py
+++ api/config.py
@@ -10,13 +10,6 @@ postgresql_config = {
 def alchemy_uri():
 	return f"postgresql://{postgresql_config['user']}:{postgresql_config['pass']}@{postgresql_config['host']}/{postgresql_config['db']}"
 
-test_db = {
-'user': 'test',
-'password': '1234',
-'host': 'localhost',
-'port': 3306,
-'database': 'test_db'
-}
 
 def test_db_uri():
 	return "sqlite:///:memory:"
